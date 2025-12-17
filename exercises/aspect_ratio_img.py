4e61c8b76973e09cb14ccd9aeed46e3c9b10ffb7
rosendocamal <josecamalt@outlook.com>
2025-12-17 01:10:25 -0500

    Refactorización de Aspect Ratio de una imagen

--- exercises/aspect_ratio_img.py
+++ exercises/aspect_ratio_img.py
@@ -12,19 +12,7 @@ import requests as req
 from PIL import Image
 from io import BytesIO
 
-# URL de la imagen.
-img_src = "https://www.python.org/static/img/python-logo.png"
-
-# Imagen obtenida para ser manipulada.
-response = req.get(img_src)
-img = Image.open(BytesIO(response.content))
-
-# Obtener el valor del ancho y alto de la imagen.
-width, height = img.width, img.height
-
-print(width, height)
-# Calculando el aspect ratio
-
+# Determinar si un número es primo o no
 def is_prime_num(num):
     if num < 2:
         return False
@@ -37,74 +25,74 @@ def is_prime_num(num):
 
     return True
 
+# Obtener una serie de números primos
 def get_nums(function, t_min, t_max):
     nums = [num for num in range(t_min, t_max + 1) if function(num)]
 
     return nums
 
-def mcd(w, h):  
-    prime_nums = get_nums(is_prime_num, 1, w)
+# Obtener el mcd del alto y ancho de una imagen
+def mcd(width, height):  
+    # Obtener los factores (divisores) de un número
+    def get_factors(num):
+        prime_nums = get_nums(is_prime_num, 1, num)
 
-    temp = w
-    divider = prime_nums[0]
-    dividers_w = list()
+        divider = prime_nums[0]
+        factors_num = list()
+
+        while num != 1:
+            if num % divider == 0:
+                num = num / divider
+                factors_num.append(divider)
+            else:
+                for i in range(1, len(prime_nums)):
+                    divider = prime_nums[i]
+                    
+                    if num % divider == 0:
+                        num = num / divider
+                        factors_num.append(divider)
+                        divider = prime_nums[0]
+                        break
+
+        factors_num.sort()
+        return factors_num
+
+    factors_width = get_factors(width) # Divisores de width
+    factors_height = get_factors(height) # Divisores de height
+
+    factors = set(factors_width + factors_height) # Todos los divisores únicos de width más los de height
     
-    while temp != 1:
-        if temp % divider == 0:
-            temp = temp / divider
-            dividers_w.append(divider)
-            divider = prime_nums[0]
-        elif temp % divider != 0:
-            for i in range(1, len(prime_nums)):
-                divider = prime_nums[i]
-                if temp % divider == 0:
-                    temp = temp / divider
-                    dividers_w.append(divider)
-                    divider = prime_nums[0]
-                    break
+    # Diccionario con todos los divisores de width y height con el formato "divisor":"exponente"
+    num_factors = { i:min(factors_width.count(i), factors_height.count(i)) for i in factors }
 
-    prime_nums = get_nums(is_prime_num, 1, h)
-
-    temp = h
-    divider = prime_nums[0]
-    dividers_h = list()
-    
-    while temp != 1:
-        if temp % divider == 0:
-            temp = temp / divider
-            dividers_h.append(divider)
-            divider = prime_nums[0]
-        elif temp % divider != 0:
-            for i in range(1, len(prime_nums)):
-                divider = prime_nums[i]
-                if temp % divider == 0:
-                    temp = temp / divider
-                    dividers_h.append(divider)
-                    divider = prime_nums[0]
-                    break 
-
-    dividers_w.sort()
-    dividers_h.sort()
-
-    dividers = set(dividers_w + dividers_h)
-    
-    total_factors = dict()
-
-    for i in dividers:
-        total_factors[i] = min(dividers_w.count(i), dividers_h.count(i))
-
+    # Mínimo común divisor
     mcd = 1
-    for i in total_factors.keys():
-        mcd *= i ** total_factors[i]
-
+    for i in num_factors.keys():
+        mcd *= i ** num_factors[i]
 
     return mcd
 
-def aspect_ratio(w, h, mcd):
-    weigth = int(w / mcd)
-    height = int(h / mcd)
 
-    print(f"The aspect ratio is {weigth}:{height}.")
+# Obtener el aspect ratio de una imagen
+def aspect_ratio(width, height, mcd):
+    width = int(width / mcd)
+    height = int(height / mcd)
+
+    print(f"The aspect ratio is {width}:{height}.")
+
+def main():
+    # URL de la imagen.
+    img_src = "https://www.python.org/static/img/python-logo.png"
+
+    # Imagen obtenida para ser manipulada.
+    response = req.get(img_src)
+    img = Image.open(BytesIO(response.content))
+
+    # Obtener el valor del ancho y alto de la imagen.
+    width, height = img.width, img.height
+
+    aspect_ratio(width, height, mcd(width, height))
 
 
-aspect_ratio(width, height, mcd(width, height))
+if __name__ == "__main__":
+    main()
